aed26e01bae3ab9c4f7aaf159e35161a37abbc03
alex-sandova1 <alex3sandova@gmail.com>
2024-03-17 23:50:44 -0700

    worked on getting profile information from firebase sample user

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:32.7.4"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore:24.10.3")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.activity:activity:1.8.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/familyconnection/Profile.kt
+++ app/src/main/java/com/example/familyconnection/Profile.kt
@@ -3,15 +3,22 @@ package com.example.familyconnection
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        val db = FirebaseFirestore.getInstance()
+        val name = "John Doe"
+        var nameInput = findViewById<TextView>(R.id.profile_name)
+        var lifestyleInput = findViewById<TextView>(R.id.Lifestyle_input)
         setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -22,5 +29,6 @@ class Profile : AppCompatActivity() {
         Return.setOnClickListener{
             startActivity(Intent(this, main_page::class.java))
         }
+
+        }
     }
-}
