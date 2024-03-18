e1dc0b83ef254245d0d72a2765c20067f28f8029
alex-sandova1 <alex3sandova@gmail.com>
2024-03-17 21:51:45 -0700

    set up profile page with back button, contains resident, full name, room #, lifestyle, Diet, allergy and contact information

--- app/src/main/java/com/example/familyconnection/MainActivity.kt
+++ app/src/main/java/com/example/familyconnection/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.familyconnection
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login = findViewById<Button>(R.id.login_button)
+        login.setOnClickListener{
+            startActivity(Intent(this, main_page::class.java))
+        }
     }
+
+
 }
--- app/src/main/java/com/example/familyconnection/Profile.kt
+++ app/src/main/java/com/example/familyconnection/Profile.kt
@@ -1,28 +1,26 @@
 package com.example.familyconnection
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
-import android.widget.ImageView
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class main_page : AppCompatActivity() {
+class Profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main_page)
-        val profile_page = findViewById<ImageView>(R.id.profile)
+        setContentView(R.layout.activity_profile)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        profile_page.setOnClickListener {
-            startActivity(Intent(this, Profile::class.java))
+        val Return = findViewById<Button>(R.id.back)
+        Return.setOnClickListener{
+            startActivity(Intent(this, main_page::class.java))
         }
-
     }
 }
