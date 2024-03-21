b1b5f136cc00e936631b244c34948afb401aaf37
alex-sandova1 <alex3sandova@gmail.com>
2024-03-20 21:01:04 -0700

    worked on salon page, salon confirmation page was done in the same style as the outing confirmation, again no functionality was given yet, only layout and basic navigation has been created

--- app/src/main/java/com/example/familyconnection/salon_confirmation.kt
+++ app/src/main/java/com/example/familyconnection/salon_confirmation.kt
@@ -8,23 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class salon : AppCompatActivity() {
+class salon_confirmation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_salon)
-        val confirm = findViewById<Button>(R.id.confirmation_button)
+        setContentView(R.layout.activity_salon_confirmation)
+        val back = findViewById<Button>(R.id.back)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val Return = findViewById<Button>(R.id.back)
-        Return.setOnClickListener {
-            startActivity(Intent(this, main_page::class.java))
-        }
-        confirm.setOnClickListener {
-            startActivity(Intent(this, salon_confirmation::class.java))
+        back.setOnClickListener {
+            startActivity(Intent(this, salon::class.java))
         }
     }
 }
