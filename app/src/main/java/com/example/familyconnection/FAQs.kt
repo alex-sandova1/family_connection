43bd5b3741d79f25b6241500c8e9270bc1ccbe6e
alex-sandova1 <alex3sandova@gmail.com>
2024-03-20 20:05:39 -0700

    FAQs page only goes between pages, this page will have information added at a later time

--- app/src/main/java/com/example/familyconnection/FAQs.kt
+++ app/src/main/java/com/example/familyconnection/FAQs.kt
@@ -1,36 +1,26 @@
 package com.example.familyconnection
 
 import android.content.Intent
-import android.media.Image
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class front_desk : AppCompatActivity() {
+class FAQs : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_front_desk)
+        setContentView(R.layout.activity_faqs)
+        val back = findViewById<Button>(R.id.back)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val Return = findViewById<Button>(R.id.back)
-        val outing = findViewById<ImageView>(R.id.outing_icon)
-        val faqs = findViewById<ImageView>(R.id.FAQs_icon)
-        Return.setOnClickListener {
-            startActivity(Intent(this, main_page::class.java))
-        }
-        outing.setOnClickListener {
-            startActivity(Intent(this, Outings::class.java))
-        }
-        faqs.setOnClickListener {
-            startActivity(Intent(this, FAQs::class.java))
+        back.setOnClickListener {
+            startActivity(Intent(this, front_desk::class.java))
         }
     }
 }
--- app/src/main/java/com/example/familyconnection/Outing_confirmation.kt
+++ app/src/main/java/com/example/familyconnection/Outing_confirmation.kt
@@ -1,6 +1,8 @@
 package com.example.familyconnection
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,14 @@ class Outing_confirmation : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_outing_confirmation)
+        val back = findViewById<Button>(R.id.back)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        back.setOnClickListener {
+            startActivity(Intent(this, front_desk::class.java))
         }
     }
 }
