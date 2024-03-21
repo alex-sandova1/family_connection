46af8033cc6a6af0909583d1ff149a31efa0a933
alex-sandova1 <alex3sandova@gmail.com>
2024-03-20 19:50:45 -0700

    finished outing sequence layout and basics, functionality will be added later

--- app/src/main/java/com/example/familyconnection/Outing_confirmation.kt
+++ app/src/main/java/com/example/familyconnection/Outing_confirmation.kt
@@ -1,31 +1,20 @@
 package com.example.familyconnection
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Outings : AppCompatActivity() {
+class Outing_confirmation : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_outings)
-        val confirmation = findViewById<Button>(R.id.confirmination_button)
-        val back = findViewById<Button>(R.id.back)
-
+        setContentView(R.layout.activity_outing_confirmation)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        confirmation.setOnClickListener{
-            startActivity(Intent(this, Outing_confirmation::class.java))
-        }
-        back.setOnClickListener{
-            startActivity(Intent(this, front_desk::class.java))
         }
     }
 }
